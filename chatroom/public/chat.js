e453512f49542c0ca6ac52be1607afd9204ba104
itachironny <42782646+itachironny@users.noreply.github.com>
2019-04-08 18:53:02 +0530

    Add files via upload

---
+++ chatroom/public/chat.js
@@ -0,0 +1,33 @@
+$(function(){
+	var d = new Date();
+	//create a socket
+	var socket = io('http://localhost:3000');
+	//connection event handler : message coming in
+	socket.on('message from server', function (msg) {
+		// body...
+		var list= $('<li>');
+		list.append($('<div>').append($('<h1>').text(msg['username'])).css({
+													'color': msg['color'], 
+													'font-size':'0.4em'
+												}));
+		list.append($('<div>').text(msg['message']));
+		list.append($('<div>').append($('<p>').text(d.toLocaleTimeString())).css({
+															'font-size':'0.7em',
+															'text-align':'right',
+															'color':'green'
+														}));
+		$('#messages').append(list);
+	});
+	// message going out
+	$('form').submit(function(event){
+		event.preventDefault();
+		var msg= {
+			'message': $('form #msg_div #m').val(),
+			'username': $('form #user #username').val(),
+			'color': $('form #user #color').val()
+		};
+		socket.emit('message from client',msg);
+		$('form #m').val('');
+		return false;
+	});
+});
