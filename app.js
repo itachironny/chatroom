ee24fd5c7a7c08f0f8fdb43265e2ffb66f621193
itachironny <42782646+itachironny@users.noreply.github.com>
2019-04-11 21:45:17 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 const express = require('express');
 const iolib = require('socket.io');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
 //const http= require('http');
 
 const app = express();
-       
 app.set('view engine','ejs');   // setting template engine as ejs
 
 //middlewares
-app.use(express.static(path.join(__dirname, 'public'))).set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 
 //routes
     //route 1
 function home(req,res){
-	res.render('index');
+	res.render('chatroomfortwo');
 }
 app.get('/',home);
 
 
 //listen on port 3000
-server= app.listen(PORT, () => console.log('Listening on ${ PORT }'));   //http.Server(app);
+server= app.listen(3000);   //http.Server(app);
 //server.listen(3000,()=>{console.log('connected at 3000')});
 
 //make a socket
 io=iolib(server);
 io.on('connection', (socket)=>{
-	//console.log('new user connected/n');
+	console.log('new user connected/n');
+	socket.on('user connection',(msg)=>{console.log(msg);});
 	socket.on('message from client',function(msg){
-		//console.log('msg received');
-		//console.log(msg);
+		console.log('msg received');
+		console.log(msg);
 		io.emit('message from server',msg);
 	});
 });
